bb82562370cb204cd62671aeebd2a087c8e432f5
LeweyWhite <luigi.bianchi@you.unipa.it>
2025-05-12 16:58:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ <script>
+function sidebar_open() {
+  document.getElementById("Sidebar").style.display = "block";
+}
+
+function sidebar_close() {
+  document.getElementById("Sidebar").style.display = "none";
+}
+
+
+
+</script>
